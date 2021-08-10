16f99ea07a9ffa65f93efbb21647daaf8ddf01cc
BFourcin <69150522+BFourcin@users.noreply.github.com>
2021-08-10 15:16:20 +0100

    pytest

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rcmodel',
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    url='https://github.com/mypackage.git',
+    packages=find_packages(where='src'),
+    package_dir={
+        '': 'src',
+)
