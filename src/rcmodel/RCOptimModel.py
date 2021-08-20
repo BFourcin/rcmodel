9f8e072fc0dcacc8949caae99197860ea8e289f2
BFourcin <69150522+BFourcin@users.noreply.github.com>
2021-08-20 16:50:31 +0100

    tidying

--- src/rcmodel/RCOptimModel.py
+++ src/rcmodel/RCOptimModel.py
@@ -6,8 +6,11 @@ class RCOptimModel(nn.Module):
     """
     Custom Pytorch model for gradient optimization.
     Initilaises with random parameters.
+
+    scaling - Class containing methods to scale inputs from 0-1 back to their usual values and back again.
+    transform - function to transform parameters e.g. sigmoid
     """
-    def __init__(self, building):
+    def __init__(self, building, scaling, transform=None):
 
         super().__init__()
         # initialize weights with random numbers, length of params given from building class
@@ -17,7 +20,12 @@ class RCOptimModel(nn.Module):
 
         self.building = building
 
-    def forward(self, Tout_continous, iv, t_eval, Scaling_func, t0=0):
+        self.transform = transform # transform - on parameters e.g. sigmoid
+
+        self.scaling = scaling #InputScaling class
+
+
+    def forward(self, Tout_continous, iv, t_eval, t0=0):
         """
         Integrates the ode forward in time.
 
@@ -25,11 +33,15 @@ class RCOptimModel(nn.Module):
         Tout_continous - A scipy interp1d function covering the whole of t_eval. Tout_continous(t) = Outside temperature at time t
         iv - Inital value. Starting temperatures of all nodes
         t_eval - times function should return a solution. e.g. torch.arange(0, 10000, 30). ensure dtype=float32
-        Scaling_func - function to scale inputs from 0-1 back to their usual values.
         t0 - starting time if not 0
         """
 
-        theta = Scaling_func(self.params)
+        if self.transform:
+            x = self.transform(self.params)
+        else:
+            x = self.params
+
+        theta = self.scaling.physical_scaling(x)
 
         # ensures iv has correct dimensions. If iv is a row vector try statement catches and converts to column vector.
         # If iv has incorrect dim then causes error downstream which is difficult to trace back.
@@ -38,7 +50,6 @@ class RCOptimModel(nn.Module):
                 raise ValueError('iv has incorrect dimensions, should be torch.Size([n, 1])')
         except IndexError:
             iv = iv.unsqueeze(1)
-
 
         xdot = self.ODE(theta, self.building, Tout_continous, t0)
         fODE = xdot.fODE
