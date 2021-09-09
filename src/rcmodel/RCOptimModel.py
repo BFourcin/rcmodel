b0159125e339632fbe37d75e584fe1e205418cb7
BFourcin <69150522+BFourcin@users.noreply.github.com>
2021-09-09 12:36:25 +0100

    Ability to select heating control on a room by room basis

--- src/rcmodel/RCOptimModel.py
+++ src/rcmodel/RCOptimModel.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from torchdiffeq import odeint
 
+
 class RCOptimModel(nn.Module):
     """
     Custom Pytorch model for gradient optimization.
@@ -13,45 +14,63 @@ class RCOptimModel(nn.Module):
     def __init__(self, building, scaling, transform=None):
 
         super().__init__()
-        # initialize weights with random numbers, length of params given from building class
-        params = torch.rand(building.n_params, dtype=torch.float32, requires_grad=True)
-        # make theta torch parameters
-        self.params = nn.Parameter(params)
-
         self.building = building
 
         self.transform = transform # transform - on parameters e.g. sigmoid
 
         self.scaling = scaling #InputScaling class
 
+        # initialize weights with random numbers, length of params given from building class
+        params = torch.rand(building.n_params, dtype=torch.float32, requires_grad=True)
+        # make theta torch parameters
+        self.params = nn.Parameter(params)
+        self.heating = nn.Parameter(torch.randn((len(building.rooms), 3), dtype=torch.float32, requires_grad=True)) # [Q, theta_A, theta_B]
 
-    def forward(self, Tout_continous, iv, t_eval, t0=0):
+        self.Q_lim = 500
+
+    def forward(self, Tout_continuous, iv, t_eval):
         """
         Integrates the ode forward in time.
 
         building - Initialised RCModel Class
-        Tout_continous - A scipy interp1d function covering the whole of t_eval. Tout_continous(t) = Outside temperature at time t
+        Tout_continuous - A scipy interp1d function covering the whole of t_eval. Tout_continuous(t) = Outside temperature at time t
         iv - Inital value. Starting temperatures of all nodes
         t_eval - times function should return a solution. e.g. torch.arange(0, 10000, 30). ensure dtype=float32
         t0 - starting time if not 0
         """
 
         if self.transform:
-            x = self.transform(self.params)
+            theta = self.transform(self.params)
+            Q_avg = self.transform(self.heating[:,0]) #W/m2 for each room
         else:
-            x = self.params
+            theta = self.params
+            Q_avg = self.heating[:,0]
 
-        theta = self.scaling.physical_scaling(x)
+        # Scale inputs up to their physical values
+        theta   = self.scaling.physical_scaling(theta)
+        Q_avg   = self.scaling.heat_scaling(Q_avg, Q_lim=self.Q_lim)
+        
+        # Get iterp1d object of Q
+        # theta_A = self.heating[:,1]
+        # theta_B = self.heating[:,2]
+
+
+        t_eval_plus = torch.cat((t_eval, (t_eval[-1]+600).unsqueeze(0)), 0)
+        Qrms_continuous = self.building.Q_continuous(t_eval_plus, Q_avg, self.heating[:,1], self.heating[:,2])
+
+        t0 = t_eval[0]
+        t_eval = t_eval - t0
+
 
         # ensures iv has correct dimensions. If iv is a row vector try statement catches and converts to column vector.
-        # If iv has incorrect dim then causes error downstream which is difficult to trace back.
+        # If iv has incorrect dim it causes error downstream which is difficult to trace back.
         try:
             if iv.shape[0] != 2+len(self.building.rooms) or iv.shape[1] != 1:
                 raise ValueError('iv has incorrect dimensions, should be torch.Size([n, 1])')
         except IndexError:
             iv = iv.unsqueeze(1)
 
-        xdot = self.ODE(theta, self.building, Tout_continous, t0)
+        xdot = self.ODE(theta, self.building, Tout_continuous, Qrms_continuous, t0)
         fODE = xdot.fODE
 
         # integrate using fixed step (rk4) see torchdiffeq docs for more options.
@@ -66,16 +85,25 @@ class RCOptimModel(nn.Module):
         dy/dx = Ax + Bu
         """
 
-        def __init__(self, theta, building, Tout_continous, t0):
+        def __init__(self, theta, building, Tout_continuous, Qrms_continuous, t0):
 
             self.building = building
-            self.A, self.Q = self.building.update_inputs(theta) #update building and get matrix A and Q for current theta
+            self.A = self.building.update_inputs(theta) #update building and get matrix A
             self.B = building.input_matrix()
-            self.Tout_continous = Tout_continous
+            self.Tout_continuous = Tout_continuous
+            self.Qrms_continuous = Qrms_continuous
             self.t0 = t0 #starting time
 
-        def fODE(self, t, x):
 
-            u = self.building.input_vector(self.Tout_continous(t.item() + self.t0), self.Q)
+        def fODE(self, t, x):
+            Tout = self.Tout_continuous(t.item() + self.t0)
+
+            Q = self.Qrms_continuous(t + self.t0)
+            # print(t)
+
+            u = self.building.input_vector(Tout, Q)
+            # print('u' , u)
+            # print('A@x ', self.A @ x)
+            # print('B@u', self.B @ u)
 
             return self.A @ x + self.B @ u
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -8,9 +8,8 @@ def scaling():
     rm_CA = [200, 800] #[min, max] Capacitance/area
     ex_C = [1.5*10**4, 10**6] #Capacitance
     R = [0.2, 1.2] # Resistance ((K.m^2)/W)
-    Q_avg = [0, 100] # Heat/area (W/m^2)
 
-    scaling = InputScaling(rm_CA, ex_C, R, Q_avg)
+    scaling = InputScaling(rm_CA, ex_C, R)
     return scaling
 
 
@@ -21,10 +20,10 @@ def test_physical_scaling(scaling):
     theta = 0.5 * torch.ones(scaling.get_n_params())
     theta_physical = scale_f(theta)
 
-    assert torch.equal(theta_physical, torch.tensor([5.0000e+02, 5.0750e+05, 5.0750e+05, 7.0000e-01, 7.0000e-01, 7.0000e-01, 7.0000e-01, 5.0000e+01]))
+    assert torch.equal(theta_physical, torch.tensor([5.0000e+02, 5.0750e+05, 5.0750e+05, 7.0000e-01, 7.0000e-01, 7.0000e-01, 7.0000e-01]))
 
 def test_model_scaling(scaling):
 
-    model_scaled = scaling.model_scaling(torch.tensor([5.0000e+02, 5.0750e+05, 5.0750e+05, 7.0000e-01, 7.0000e-01, 7.0000e-01, 7.0000e-01, 5.0000e+01]))
+    model_scaled = scaling.model_scaling(torch.tensor([5.0000e+02, 5.0750e+05, 5.0750e+05, 7.0000e-01, 7.0000e-01, 7.0000e-01, 7.0000e-01]))
 
     assert torch.equal(model_scaled, 0.5*torch.ones(scaling.get_n_params()))
