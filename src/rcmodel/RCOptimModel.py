5f1f87d2232bb26f30b212dd42cdfd8d11d629be
BFourcin <69150522+BFourcin@users.noreply.github.com>
2021-09-21 18:59:59 +0100

    Update RCOptimModel.py

--- src/rcmodel/RCOptimModel.py
+++ src/rcmodel/RCOptimModel.py
@@ -11,7 +11,7 @@ class RCOptimModel(nn.Module):
     scaling - Class containing methods to scale inputs from 0-1 back to their usual values and back again.
     transform - function to transform parameters e.g. sigmoid
     """
-    def __init__(self, building, scaling, transform=None):
+    def __init__(self, building, scaling, Tout_continuous, transform=None):
 
         super().__init__()
         self.building = building
@@ -19,6 +19,8 @@ class RCOptimModel(nn.Module):
         self.transform = transform # transform - on parameters e.g. sigmoid
 
         self.scaling = scaling #InputScaling class
+
+        self.Tout_continuous = Tout_continuous #Interp1D object
 
         # initialize weights with random numbers, length of params given from building class
         params = torch.rand(building.n_params, dtype=torch.float32, requires_grad=True)
@@ -28,7 +30,8 @@ class RCOptimModel(nn.Module):
 
         self.Q_lim = 500
 
-    def forward(self, Tout_continuous, iv, t_eval):
+
+    def forward(self, t_eval):
         """
         Integrates the ode forward in time.
 
@@ -38,6 +41,11 @@ class RCOptimModel(nn.Module):
         t_eval - times function should return a solution. e.g. torch.arange(0, 10000, 30). ensure dtype=float32
         t0 - starting time if not 0
         """
+
+        #check if t_eval is formatted correctly:
+        if t_eval.dim() > 1:
+            t_eval = t_eval.squeeze(0)
+
 
         if self.transform:
             theta = self.transform(self.params)
@@ -49,7 +57,7 @@ class RCOptimModel(nn.Module):
         # Scale inputs up to their physical values
         theta   = self.scaling.physical_scaling(theta)
         Q_avg   = self.scaling.heat_scaling(Q_avg, Q_lim=self.Q_lim)
-        
+
         # Get iterp1d object of Q
         # theta_A = self.heating[:,1]
         # theta_B = self.heating[:,2]
@@ -61,22 +69,19 @@ class RCOptimModel(nn.Module):
         t0 = t_eval[0]
         t_eval = t_eval - t0
 
+        # set initial value as 4 degrees higher than current outside temp
+        iv = (self.Tout_continuous(torch.tensor(t0.item())) + 4) * torch.ones(2+len(self.building.rooms))
+        # iv = (self.Tout_continuous(torch.tensor(t0.item(), device=device)) + 4) * torch.ones(2+len(self.building.rooms), device=device)
+        iv = iv.unsqueeze(1) #set iv as column vector. Errors caused if Row vector which are difficult to trace.
 
-        # ensures iv has correct dimensions. If iv is a row vector try statement catches and converts to column vector.
-        # If iv has incorrect dim it causes error downstream which is difficult to trace back.
-        try:
-            if iv.shape[0] != 2+len(self.building.rooms) or iv.shape[1] != 1:
-                raise ValueError('iv has incorrect dimensions, should be torch.Size([n, 1])')
-        except IndexError:
-            iv = iv.unsqueeze(1)
 
-        xdot = self.ODE(theta, self.building, Tout_continuous, Qrms_continuous, t0)
+        xdot = self.ODE(theta, self.building, self.Tout_continuous, Qrms_continuous, t0)
         fODE = xdot.fODE
 
         # integrate using fixed step (rk4) see torchdiffeq docs for more options.
         integrate = odeint(fODE, iv, t_eval, method='rk4') #https://github.com/rtqichen/torchdiffeq
 
-        return integrate
+        return integrate[:,2:] #first two columns are removed, they're external wall nodes. 
 
 
     class ODE():
