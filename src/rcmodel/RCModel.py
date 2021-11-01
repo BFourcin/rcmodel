3bb565f257bfcd941e8f463a581b29da755928dd
BFourcin <69150522+BFourcin@users.noreply.github.com>
2021-11-01 00:14:27 +0000

    Formatting change

--- src/rcmodel/RCModel.py
+++ src/rcmodel/RCModel.py
@@ -14,32 +14,32 @@ class RCModel(nn.Module):
 
     Runs using foward method should be sequential as the output is used as the inital conditon for the next run, unless manually reset using self.iv
     """
+
     def __init__(self, building, scaling, Tout_continuous, transform=None, cooling_policy=None):
 
         super().__init__()
         self.building = building
 
-        self.transform = transform # transform - on parameters e.g. sigmoid
+        self.transform = transform  # transform - on parameters e.g. sigmoid
 
-        self.scaling = scaling #InputScaling class (helper class to go between machine (0-1) and physical values)
+        self.scaling = scaling  # InputScaling class (helper class to go between machine (0-1) and physical values)
 
-        self.Tout_continuous = Tout_continuous #Interp1D object
+        self.Tout_continuous = Tout_continuous  # Interp1D object
 
-        self.cooling_policy = cooling_policy #Neural net: cooling_policy(state) --> action
+        self.cooling_policy = cooling_policy  # Neural net: cooling_policy(state) --> action
 
         # initialize weights with random numbers, length of params given from building class
         params = torch.rand(building.n_params, dtype=torch.float32, requires_grad=True)
         # make theta torch parameters
         self.params = nn.Parameter(params)
-        self.heating = nn.Parameter(torch.randn((len(building.rooms), 3), dtype=torch.float32, requires_grad=True)) # [Q, theta_A, theta_B]
+        self.heating = nn.Parameter(
+            torch.randn((len(building.rooms), 3), dtype=torch.float32, requires_grad=True))  # [Q, theta_A, theta_B]
 
         self.Q_lim = 500
 
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-        self.reset_iv() #sets iv
-
-        
+        self.reset_iv()  # sets iv
 
     def forward(self, t_eval):
         """
@@ -52,45 +52,44 @@ class RCModel(nn.Module):
         t0 - starting time if not 0
         """
 
-        #check if t_eval is formatted correctly:
+        # check if t_eval is formatted correctly:
         if t_eval.dim() > 1:
             t_eval = t_eval.squeeze(0)
 
         # Check if Cuda is available
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-        #Transform parameters
+        # Transform parameters
         if self.transform:
             theta = self.transform(self.params)
-            Q_avg = self.transform(self.heating[:,0]) #W/m2 for each room
+            Q_avg = self.transform(self.heating[:, 0])  # W/m2 for each room
         else:
             theta = self.params
-            Q_avg = self.heating[:,0]
+            Q_avg = self.heating[:, 0]
 
         # Scale inputs up to their physical values
-        theta   = self.scaling.physical_scaling(theta)
-        Q_avg   = self.scaling.heat_scaling(Q_avg, Q_lim=self.Q_lim)
+        theta = self.scaling.physical_scaling(theta)
+        Q_avg = self.scaling.heat_scaling(Q_avg, Q_lim=self.Q_lim)
 
-        t_eval_plus = torch.cat((t_eval, (t_eval[-1]+600).unsqueeze(0)), 0) #added on extra time to avoide error with interp1d(t_end)
-        Qrms_continuous = self.building.Q_continuous(t_eval_plus, Q_avg, self.heating[:,1], self.heating[:,2])
+        t_eval_plus = torch.cat((t_eval, (t_eval[-1] + 600).unsqueeze(0)),
+                                0)  # added on extra time to avoide error with interp1d(t_end)
+        Qrms_continuous = self.building.Q_continuous(t_eval_plus, Q_avg, self.heating[:, 1], self.heating[:, 2])
 
         # t0 stores the starting epoch time and t_eval is array of seconds from start, [0, 1*dt, 2*dt, ...]
         self.t0 = t_eval[0]
         t_eval = t_eval - self.t0
 
-        #Produce matrix A and B from current parameters
+        # Produce matrix A and B from current parameters
         self.A = self.building.update_inputs(theta).to(self.device)
         self.B = self.building.input_matrix().to(self.device)
 
         # integrate using fixed step (rk4) see torchdiffeq docs for more options.
-        integrate = odeint(self.fODE, self.iv, t_eval, method='rk4') #https://github.com/rtqichen/torchdiffeq
+        integrate = odeint(self.fODE, self.iv, t_eval, method='rk4')  # https://github.com/rtqichen/torchdiffeq
 
-        #get last ouput and use for next initial value
+        # get last ouput and use for next initial value
         self.iv = integrate[-1, :]
 
-        return integrate[:,2:] #first two columns are removed, they're external wall nodes.
-
-
+        return integrate  # first two columns are external envelope nodes. i.e not rooms
 
     def decimal_time(self, unix_epoch):
         """Time to percentage of year, week and day"""
@@ -98,13 +97,12 @@ class RCModel(nn.Module):
         date = datetime.fromtimestamp(unix_epoch)
         _, isoweek, isoweekday = date.isocalendar()
 
-        time_of_year = isoweek/52
-        time_of_week = (isoweekday-1)/7 + (date.hour*60**2 + date.minute*60 + date.second)/(60**2*24*7)
-        time_of_day = (date.hour*60**2 + date.minute*60 + date.second)/(60**2*24)
+        time_of_year = isoweek / 52
+        time_of_week = (isoweekday - 1) / 7 + (date.hour * 60 ** 2 + date.minute * 60 + date.second) / (
+                60 ** 2 * 24 * 7)
+        time_of_day = (date.hour * 60 ** 2 + date.minute * 60 + date.second) / (60 ** 2 * 24)
 
         return time_of_year, time_of_week, time_of_day
-
-
 
     def fODE(self, t, x):
         """
@@ -114,21 +112,27 @@ class RCModel(nn.Module):
 
         # cool_param: [x , time_of_week, time_of_day]
 
-
         time_of_year, time_of_week, time_of_day = self.decimal_time(t.item() + self.t0.item())
 
         mu = 23.359
         std_dev = 1.41
 
-        x_norm = (x[2:]-mu)/std_dev
+        x_norm = (x - mu) / std_dev
         x_norm = torch.reshape(x_norm, (-1,))
 
-        cool_param = torch.cat((x_norm, torch.tensor([time_of_week, time_of_day], device=self.device)))
+        # Change this to Sin(t/freq), Cos(t/freq)
+        state_cool = torch.cat(
+            (x_norm, torch.tensor([time_of_week, time_of_day], device=self.device, dtype=torch.float32)))
 
-        value = -16914
+        value = -16914  # value is learnt by first optimiser
         if self.cooling_policy:
-            Q = value * self.cooling_policy.predict(cool_param.float())
-        else:
+            action, log_prob = self.cooling_policy.get_action(state_cool)  # might need state_cool.float()
+
+            if self.cooling_policy.training:  # in training mode
+                self.cooling_policy.log_probs_buffer.append(log_prob)
+
+            Q = torch.ones(len(self.building.rooms)) * value * action
+        else:  # No cooling
             Q = torch.zeros(len(self.building.rooms))
 
         # Q = self.Qrms_continuous(t + self.t0)
@@ -138,8 +142,8 @@ class RCModel(nn.Module):
 
         return self.A @ x + self.B @ u
 
-
     def reset_iv(self):
-        #Starting Temperatures of nodes. Column vector of shape ([n,1]) n=rooms+2
-        self.iv = 26 * torch.ones(2+len(self.building.rooms), device=self.device) #set iv at 26 degrees
-        self.iv = self.iv.unsqueeze(1) #set iv as column vector. Errors caused if Row vector which are difficult to trace.
+        # Starting Temperatures of nodes. Column vector of shape ([n,1]) n=rooms+2
+        self.iv = 26 * torch.ones(2 + len(self.building.rooms), device=self.device)  # set iv at 26 degrees
+        self.iv = self.iv.unsqueeze(
+            1)  # set iv as column vector. Errors caused if Row vector which are difficult to trace.
