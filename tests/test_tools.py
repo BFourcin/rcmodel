1abf259423fd094f62a1ca70961a2e8d04703730
BFourcin <69150522+BFourcin@users.noreply.github.com>
2022-03-11 19:06:16 +0000

    update tests

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -9,7 +9,7 @@ def scaling():
     rm_CA = [200, 800]  # [min, max] Capacitance/area
     ex_C = [1.5 * 10 ** 4, 10 ** 6]  # Capacitance
     R = [0.2, 1.2]  # Resistance ((K.m^2)/W)
-    Q_limit = [-300, 500]  # gain limit (W/m^2)
+    Q_limit = [500]  # gain limit (W/m^2)
 
     scaling = InputScaling(rm_CA, ex_C, R, Q_limit)
     return scaling
@@ -20,12 +20,12 @@ def test_physical_param_scaling(scaling):
     theta_physical = scaling.physical_param_scaling(theta)
     print(theta_physical)
     assert torch.equal(theta_physical, torch.tensor(
-        [5.0000e+02, 5.0750e+05, 5.0750e+05, 7.0000e-01, 7.0000e-01, 7.0000e-01, 7.0000e-01, 1.0000e+02]))
+        [5.0000e+02, 5.0750e+05, 5.0750e+05, 7.0000e-01, 7.0000e-01, 7.0000e-01, 7.0000e-01]))
 
 
 def test_model_param_scaling(scaling):
     model_scaled = scaling.model_param_scaling(
-        torch.tensor([5.0000e+02, 5.0750e+05, 5.0750e+05, 7.0000e-01, 7.0000e-01, 7.0000e-01, 7.0000e-01, 1.0000e+02]))
+        torch.tensor([5.0000e+02, 5.0750e+05, 5.0750e+05, 7.0000e-01, 7.0000e-01, 7.0000e-01, 7.0000e-01]))
 
     assert torch.equal(model_scaled, 0.5 * torch.ones(scaling.get_n_params()))
 
