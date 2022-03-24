7328761ed644edcb9f43262684ab6648d2d02a4f
BFourcin <69150522+BFourcin@users.noreply.github.com>
2022-03-24 13:04:37 +0000

    Convenience update

--- src/rcmodel/tools/ray_actor.py
+++ src/rcmodel/tools/ray_actor.py
@@ -6,7 +6,8 @@ from matplotlib import pyplot as plt
 from tqdm.auto import tqdm, trange
 from pathlib import Path
 
-from ..tools import InputScaling, BuildingTemperatureDataset, pltsolution_1rm
+from ..tools import InputScaling, BuildingTemperatureDataset, pltsolution_1rm,\
+    exponential_smoothing, convergence_criteria
 
 
 class RayActor:
@@ -23,6 +24,70 @@ class RayActor:
     def worker(self, opt_id, epochs=100):
         from ..optimisation.optimise_rc import OptimiseRC  # placing here avoids circular import
 
+        def do_plots():
+            if self.physical_training:
+                # find smoothed curves:
+                phys_smooth = exponential_smoothing(avg_train_loss_plot, conv_alpha, n=conv_win_len)
+
+
+                # Plot Loss:
+                fig, axs = plt.subplots(figsize=(10, 7))
+                ax2 = axs.twinx()
+                ln1 = axs.plot(range(1, len(avg_train_loss_plot) + 1), avg_train_loss_plot, label='train loss')
+                ln2 = axs.plot(range(1, len(avg_test_loss_plot) + 1), avg_test_loss_plot, label='test loss')
+                ln3 = axs.plot(range(1, len(phys_smooth) + 1), phys_smooth, 'g', label='train loss - smoothed')
+                c = []
+                for i in range(len(phys_smooth)):
+                    c.append(convergence_criteria(phys_smooth[0:i + 1], conv_win_len))
+
+                ln4 = ax2.plot(range(1, len(phys_smooth) + 1), c, 'k--', label='convergence')
+
+                fig.suptitle('Train and Test Loss During Training', fontsize=16)
+                axs.set_xlabel('Epoch')
+                axs.set_ylabel('Mean Squared Error')
+                axs.set_yscale('log')
+                ax2.set_ylabel('Convergence')
+                ax2.set_yscale('log')
+                # legend:
+                lns = ln1 + ln2 + ln3 + ln4
+                labs = [l.get_label() for l in lns]
+                axs.legend(lns, labs, loc=0)
+
+                fig.savefig(f'./outputs/run{opt_id}/plots/LossPlot.png')
+                plt.close()
+
+            if self.policy_training:
+                pol_smooth = exponential_smoothing(rewards_plot, conv_alpha, n=conv_win_len)
+                # Plot Rewards:
+                fig, axs = plt.subplots(figsize=(10, 7))
+                ax2 = axs.twinx()
+
+                y = np.array(rewards_plot, dtype=np.float32)
+                ln1 = axs.plot(range(1, len(y) + 1), -y, label='total rewards')
+                ln2 = axs.plot(range(1, len(pol_smooth) + 1), -np.array(pol_smooth, dtype=np.float32), 'g',
+                               label='rewards - smoothed')
+                c = []
+                for i in range(len(pol_smooth)):
+                    c.append(convergence_criteria(pol_smooth[0:i + 1], conv_win_len))
+
+                ln3 = ax2.plot(range(1, len(pol_smooth) + 1), c, 'k--', label='convergence')
+
+                # legend:
+                lns = ln1 + ln2 + ln3
+                labs = [l.get_label() for l in lns]
+                axs.legend(lns, labs, loc=0)
+
+                fig.suptitle('Rewards', fontsize=16)
+                axs.set_xlabel('Epoch')
+                axs.set_ylabel('Reward')
+                axs.set_yscale('log')
+
+                ax2.set_ylabel('Convergence')
+                ax2.set_yscale('log')
+
+                fig.savefig(f'./outputs/run{opt_id}/plots/RewardsPlot.png')
+                plt.close()
+
         torch.set_num_threads(1)
 
         dir_path = f'./outputs/run{opt_id}/models/'  # where to save
@@ -31,6 +96,8 @@ class RayActor:
 
         dt = 30
         sample_size = 24 * 60 ** 2 / dt
+        conv_win_len = 10  # MUST BE EVEN. total length of lookback window to measure convergence
+        conv_alpha = 0.15
 
         op = OptimiseRC(self.model, self.csv_path, sample_size, dt, lr=1e-3, opt_id=opt_id)
 
@@ -77,6 +144,8 @@ class RayActor:
             if (epoch + 1) % 10 == 0 or epoch == epochs - 1 or epoch == 0:
                 pltsolution_1rm(self.model, plot_dataloader,
                                 f'./outputs/run{opt_id}/plots/results/Result_Epoch{epoch + 1}.png')
+                if epoch > 0:
+                    do_plots()
 
             # Save Model
             model_id = epoch + 1
@@ -91,38 +160,7 @@ class RayActor:
         final_train_loss = avg_train_loss
         final_test_loss = avg_test_loss
 
-        _ = plt.figure(figsize=(10, 7), dpi=400)
-        plt.plot(range(1, epochs + 1), avg_train_loss_plot, label='train loss')
-        plt.plot(range(1, epochs + 1), avg_test_loss_plot, label='test loss')
-        plt.title('Train and Test Loss During Training')
-        plt.xlabel('Epoch')
-        plt.ylabel('Mean Squared Error')
-        plt.legend()
-        plt.savefig(f'./outputs/run{opt_id}/plots/LossPlot.png')
-        plt.close()
-
-        if self.policy_training:
-            fig, axs = plt.subplots(1, 2, figsize=(10, 7), dpi=400)
-            axs[0].plot(range(1, epochs + 1), torch.flatten(torch.tensor(ER_plot)).detach().numpy(), 'b',
-                        label='expected rewards')
-            axs[0].legend()
-
-            axs[1].plot(range(1, epochs + 1), torch.flatten(torch.tensor(rewards_plot)).detach().numpy(), 'r',
-                        label='total rewards')
-            axs[1].legend()
-            fig.suptitle('Rewards', fontsize=16)
-            axs[0].set_xlabel('Epoch')
-            axs[1].set_xlabel('Epoch')
-            axs[0].set_ylabel('Reward')
-            axs[1].set_ylabel('Reward')
-
-            plt.savefig(f'./outputs/run{opt_id}/plots/RewardsPlot.png')
-            plt.close()
-
-        final_params = self.model.transform(self.model.params).detach().numpy()
-        final_cooling = self.model.transform(self.model.loads).detach().numpy()
-
-        return np.concatenate(([opt_id], final_params, final_cooling, [final_train_loss, final_test_loss]))
+        return np.concatenate(([opt_id], final_params, gain_load, cool_load, [final_train_loss, final_test_loss]))
 
 
 if __name__ == '__main__':
