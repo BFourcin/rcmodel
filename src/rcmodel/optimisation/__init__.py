1badb6638454c1cd7402d3189e32f2efaaa06906
BFourcin <69150522+BFourcin@users.noreply.github.com>
2022-10-27 15:35:25 +0100

    Add env_creator to init

--- src/rcmodel/optimisation/__init__.py
+++ src/rcmodel/optimisation/__init__.py
@@ -1,20 +1,20 @@
 from .optimise_rc import OptimiseRC
 from .optimise_rc import DDPOptimiseRC
-# from .reinforce import Reinforce
 from .environment import PolicyNetwork
 from .environment import LSIEnv
 from .environment import PriorEnv
 from .environment import PreprocessEnv
+from .environment import env_creator
 from .prior_cooling_policy import PriorCoolingPolicy
 
 
 __all__ = [
     "OptimiseRC",
     "DDPOptimiseRC",
-    # "Reinforce",
     "PolicyNetwork",
     "LSIEnv",
     "PriorEnv",
     "PriorCoolingPolicy",
+    "env_creator",
     "PreprocessEnv",
 ]
