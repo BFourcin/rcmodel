5fc659dd386aa3264e976d64cba62ec788f088b7
BFourcin <69150522+BFourcin@users.noreply.github.com>
2023-03-13 10:25:01 +0000

    Update optimise_rc.py

--- src/rcmodel/optimisation/optimise_rc.py
+++ src/rcmodel/optimisation/optimise_rc.py
@@ -5,96 +5,182 @@ import datetime
 import torch.distributed as dist
 import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel as DDP
+from ray.rllib.models.preprocessors import NoPreprocessor, get_preprocessor
+from ray.rllib.algorithms import Algorithm
 
 
-def train(model, dataloader, optimizer):
+import rcmodel.tools
+
+
+def sample_action(rl_algorithm, obs):
+    """
+    Sample action from policy distribution.
+    Obs must be pre-processed if needed.
+    For more info: https://docs.ray.io/en/latest/rllib/rllib-training.html#accessing-model-state
+    """
+    # Run a forward pass to get model output logits. Note that complex observations
+    # must be preprocessed as in the above code block.
+    logits, _ = rl_algorithm.model({"obs": torch.tensor(obs).unsqueeze(0)})
+
+    # Compute action distribution given logits
+    dist = rl_algorithm.dist_class(logits, rl_algorithm.model)
+
+    # Query the distribution for samples, sample logp
+    action = dist.sample()
+
+    # logp = dist.logp(torch.tensor(1))
+
+    return action
+
+
+def train(env, policy, optimizer):
     """
     Performs one epoch of training.
     Order of rooms in building and in data must match otherwise model will fit wrong rooms to data.
     """
+    model = env.RC
+
+    # Perform integration across entire timeseries to get initial value array
+    model.iv_array = rcmodel.tools.get_iv_array(model, env.dataloader.dataset)
 
     # Check if DDP
     if type(model) is torch.nn.parallel.distributed.DistributedDataParallel:
         ddp_model = model
         model = ddp_model.module
 
-    model.reset_iv()  # Reset initial value
     model.train()
+    num_batches = len(env.dataloader)
 
-    # if policy exists:
-    if model.cooling_policy:
-        model.cooling_policy.eval()
+    episode_reward = 0
+    done = False
+    env.env.collect_rc_grad = True
+    obs = env.reset()
+    while not done:
+        action
+        obs, reward, done, info = env.step(action)
 
-        # This ended up causing the policy not to optimise:
-        # Stops Autograd endlessly keeping track of the graph. Memory Leak!
-        # try:
-        #     for layer in model.cooling_policy.parameters():
-        #         layer.requires_grad = False
-        #
-        # # This occurs when policy contains no parameters e.g. when using prior to train.
-        # except AttributeError:
-        #     pass
-
-    num_cols = len(model.building.rooms)  # number of columns to use from data.
-    num_batches = len(dataloader)
-    train_loss = 0
-    loss_fn = torch.nn.MSELoss()
-    sum_loss = 0
-
-    for batch, (time, temp) in enumerate(dataloader):
-        # Get model arguments:
-        time = time.squeeze(0)
-        temp = temp.squeeze(0)
-
-        # Compute prediction and loss
-        pred = model(time)
-        pred = pred.squeeze(-1)  # change from column to row matrix
-
-        loss = loss_fn(pred[:, 2:], temp[:, 0:num_cols])
-        sum_loss += loss/num_batches
-
-        # get last output and use for next initial value
-        # model.iv = pred[-1, :].unsqueeze(1).detach()  # MUST DETACH GRAD
+        episode_reward += reward / num_batches
 
     # Backpropagation
     optimizer.zero_grad()
-    sum_loss.backward()
+    episode_reward.backward()
     optimizer.step()
 
-    return sum_loss.item()
+    return episode_reward.item()
 
 
-def test(model, dataloader):
+# def train(model, dataloader, optimizer):
+#     """
+#     Performs one epoch of training.
+#     Order of rooms in building and in data must match otherwise model will fit wrong rooms to data.
+#     """
+#
+#     # Check if DDP
+#     if type(model) is torch.nn.parallel.distributed.DistributedDataParallel:
+#         ddp_model = model
+#         model = ddp_model.module
+#
+#     model.train()
+#
+#     # if policy exists:
+#     if model.cooling_policy:
+#         model.cooling_policy.eval()
+#
+#         # This ended up causing the policy not to optimise:
+#         # Stops Autograd endlessly keeping track of the graph. Memory Leak!
+#         # try:
+#         #     for layer in model.cooling_policy.parameters():
+#         #         layer.requires_grad = False
+#         #
+#         # # This occurs when policy contains no parameters e.g. when using prior to train.
+#         # except AttributeError:
+#         #     pass
+#
+#     num_cols = len(model.building.rooms)  # number of columns to use from data.
+#     num_batches = len(dataloader)
+#     train_loss = 0
+#     loss_fn = torch.nn.MSELoss()
+#     sum_loss = 0
+#
+#     for batch, (time, temp) in enumerate(dataloader):
+#         # Get model arguments:
+#         time = time.squeeze(0)
+#         temp = temp.squeeze(0)
+#
+#         # Compute prediction and loss
+#         pred = model(time)
+#         pred = pred.squeeze(-1)  # change from column to row matrix
+#
+#         loss = loss_fn(pred[:, 2:], temp[:, 0:num_cols])
+#         sum_loss += loss/num_batches
+#
+#         # get last output and use for next initial value
+#         # model.iv = pred[-1, :].unsqueeze(1).detach()  # MUST DETACH GRAD
+#
+#     # Backpropagation
+#     optimizer.zero_grad()
+#     sum_loss.backward()
+#     optimizer.step()
+#
+#     return sum_loss.item()
+
+
+def test(env, rl_algorithm):
+    model = env.RC
+
     # Check if DDP
     if type(model) is torch.nn.parallel.distributed.DistributedDataParallel:
         ddp_model = model
         model = ddp_model.module
 
-    model.reset_iv()  # Reset initial value
     model.eval()  # Put model in evaluation mode
-    num_batches = len(dataloader)
-    num_cols = len(model.building.rooms)  # number of columns to take from data.
-    test_loss = 0
-    loss_fn = torch.nn.MSELoss()
+    num_batches = len(env.dataloader)
 
+    episode_reward = 0
+    done = False
+    obs = env.reset()
     with torch.no_grad():
-        for (time, temp) in dataloader:
-            time = time.squeeze(0)
-            temp = temp.squeeze(0)
+        while not done:
+            action = rl_algorithm.compute_single_action(obs)
 
-            pred = model(time)
-            pred = pred.squeeze(-1)  # change from column to row matrix
-            test_loss += loss_fn(pred[:, 2:], temp[:, 0:num_cols]).item()
+            obs, reward, done, info = env.step(action)
 
-            # get last output and use for next initial value
-            model.iv = pred[-1, :].unsqueeze(1).detach()  # MUST DETACH GRAD
+            episode_reward += reward / num_batches
 
-    test_loss /= num_batches
-
-    return test_loss
+    return episode_reward.item()
 
 
-def setup_process(rank, world_size, backend='gloo'):
+# def test(model, dataloader):
+#     # Check if DDP
+#     if type(model) is torch.nn.parallel.distributed.DistributedDataParallel:
+#         ddp_model = model
+#         model = ddp_model.module
+#
+#     model.reset_iv()  # Reset initial value
+#     model.eval()  # Put model in evaluation mode
+#     num_batches = len(dataloader)
+#     num_cols = len(model.building.rooms)  # number of columns to take from data.
+#     test_loss = 0
+#     loss_fn = torch.nn.MSELoss()
+#
+#     with torch.no_grad():
+#         for (time, temp) in dataloader:
+#             time = time.squeeze(0)
+#             temp = temp.squeeze(0)
+#
+#             pred = model(time)
+#             pred = pred.squeeze(-1)  # change from column to row matrix
+#             test_loss += loss_fn(pred[:, 2:], temp[:, 0:num_cols]).item()
+#
+#             # get last output and use for next initial value
+#             model.iv = pred[-1, :].unsqueeze(1).detach()  # MUST DETACH GRAD
+#
+#     test_loss /= num_batches
+#
+#     return test_loss
+
+
+def setup_process(rank, world_size, backend="gloo"):
     """
     Initialize the distributed environment (for each process).
 
@@ -103,18 +189,23 @@ def setup_process(rank, world_size, backend='gloo'):
     """
     # set up the master's ip address so this child process can coordinate
     # os.environ['MASTER_ADDR'] = '127.0.0.1'
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'  # '12355'
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12355"  # '12355'
 
     # - use NCCL if you are using gpus: https://pytorch.org/tutorials/intermediate/dist_tuto.html#communication-backends
     # if torch.cuda.is_available():
     #     backend = 'nccl'
     # Initializes the default distributed process group, and this will also initialize the distributed package.
-    dist.init_process_group(backend, timeout=datetime.timedelta(seconds=10), rank=rank, world_size=world_size)
+    dist.init_process_group(
+        backend,
+        timeout=datetime.timedelta(seconds=10),
+        rank=rank,
+        world_size=world_size,
+    )
 
 
 def cleanup():
-    """ Destroy a given process group, and deinitialize the distributed package """
+    """Destroy a given process group, and deinitialize the distributed package"""
     dist.destroy_process_group()
 
 
@@ -139,25 +230,39 @@ class OptimiseRC:
     see https://docs.ray.io/en/latest/using-ray-with-pytorch.html
     """
 
-    def __init__(self, model, train_dataset, test_dataset, lr=1e-3, opt_id=0):
-        self.model = model
+    def __init__(self, env_config, ppo_checkpoint_path, train_dataset, test_dataset, lr=1e-3, opt_id=0):
+
+        self.env = rcmodel.tools.env_creator(env_config)
+        self.rl_algorithm = Algorithm.from_checkpoint(ppo_checkpoint_path)
         self.lr = lr
         # self.model.init_physical()  # randomise parameters - DO WE WANT THIS?
         self.model_id = opt_id
         self.train_dataset = train_dataset
-        self.train_dataloader = torch.utils.data.DataLoader(self.train_dataset, batch_size=1, shuffle=False)
+        self.train_dataloader = torch.utils.data.DataLoader(
+            self.train_dataset, batch_size=1, shuffle=False
+        )
         self.test_dataset = test_dataset
-        self.test_dataloader = torch.utils.data.DataLoader(self.test_dataset, batch_size=1, shuffle=False)
+        self.test_dataloader = torch.utils.data.DataLoader(
+            self.test_dataset, batch_size=1, shuffle=False
+        )
 
-        self.optimizer = torch.optim.Adam([self.model.params, self.model.loads], lr=lr)
+        self.optimizer = torch.optim.Adam(
+            [self.env.RC.params, self.env.RC.loads], lr=lr, maximize=True
+        )
+
+        # Check that we don't need preprocessing.
+        prep = get_preprocessor(self.env.observation_space)
+        assert prep.__name__ == 'NoPreprocessor', 'Not implemented preprocessing.'
 
     def train(self):
-        avg_loss = train(self.model, self.train_dataloader, self.optimizer)
-        return avg_loss
+        self.env.dataloader = self.train_dataloader
+        avg_reward = train(self.env, self.rl_algorithm, self.optimizer)
+        return avg_reward
 
     def test(self):
-        test_loss = test(self.model, self.test_dataloader)
-        return test_loss
+        self.env.dataloader = self.test_dataloader
+        avg_reward = test(self.env, self.rl_algorithm)
+        return avg_reward
 
 
 class DDPOptimiseRC:
@@ -214,17 +319,27 @@ class DDPOptimiseRC:
 
         setup_process(rank, world_size)
 
-        sampler = torch.utils.data.DistributedSampler(dataset, num_replicas=world_size, rank=rank, shuffle=False,
-                                                      drop_last=True)
+        sampler = torch.utils.data.DistributedSampler(
+            dataset, num_replicas=world_size, rank=rank, shuffle=False, drop_last=True
+        )
 
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, pin_memory=False, num_workers=0,
-                                                 drop_last=True, shuffle=False, sampler=sampler)
+        dataloader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=1,
+            pin_memory=False,
+            num_workers=0,
+            drop_last=True,
+            shuffle=False,
+            sampler=sampler,
+        )
 
         # create distributed model
         ddp_model = DDP(self.model, find_unused_parameters=True)
 
         if func is train:
-            optimizer = torch.optim.Adam([ddp_model.module.params, ddp_model.module.loads], lr=self.lr)
+            optimizer = torch.optim.Adam(
+                [ddp_model.module.params, ddp_model.module.loads], lr=self.lr
+            )
             loss = func(ddp_model, dataloader, optimizer)
         elif func is test:
             loss = func(ddp_model, dataloader)
@@ -248,7 +363,10 @@ class DDPOptimiseRC:
 
         result_queue = mp.Queue()
         for rank in range(world_size):
-            mp.Process(target=self.parallel_loop, args=(rank, world_size, func, dataset, result_queue)).start()
+            mp.Process(
+                target=self.parallel_loop,
+                args=(rank, world_size, func, dataset, result_queue),
+            ).start()
 
         avg_loss = result_queue.get()
 
@@ -261,5 +379,3 @@ class DDPOptimiseRC:
     def test(self):
         avg_loss = self.run_parallel_loop(test, self.test_dataset)
         return avg_loss
-
-
