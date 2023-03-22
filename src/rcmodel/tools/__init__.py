7ae5ef6fbbeba55f6edeebbbe52ed1dbd437c91c
BFourcin <69150522+BFourcin@users.noreply.github.com>
2023-03-22 10:28:08 +0000

    add InfiniteSampler and env_create_and_setup

--- src/rcmodel/tools/__init__.py
+++ src/rcmodel/tools/__init__.py
@@ -1,9 +1,11 @@
 from .plotting import pltsolution_1rm
 from .rcmodel_dataset import BuildingTemperatureDataset
 from .rcmodel_dataset import RandomSampleDataset
+from .rcmodel_dataset import InfiniteSampler
 from .helper_functions import initialise_model
 from .helper_functions import model_creator
 from .helper_functions import env_creator
+from .helper_functions import env_create_and_setup
 from .helper_functions import model_to_csv
 from .helper_functions import convergence_criteria
 from .helper_functions import exponential_smoothing
@@ -17,8 +19,10 @@ __all__ = [
     "pltsolution_1rm",
     "BuildingTemperatureDataset",
     "RandomSampleDataset",
+    "InfiniteSampler",
     "model_creator",
     "env_creator",
+    "env_create_and_setup",
     "initialise_model",
     "model_to_csv",
     "convergence_criteria",
